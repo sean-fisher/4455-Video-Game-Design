0c6d0ab6c00c4619cbbf6c0aa9ef3afa92f26df3
Justin Weltner-Evans <jtwe3@gatech.edu>
2019-02-26 13:17:04 -0500

    Moving Platform and Rotating Cylinder Prefabs that auto run when placed in scene

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+	public Transform platform;
+	public Transform start;
+	public Transform end;
+
+	public float speed;
+
+	Vector3 direction;
+	Transform destination;
+
+	void Start() {
+		SetDest(start);
+	}
+
+	void FixedUpdate() {
+		platform.GetComponent<Rigidbody>().MovePosition(platform.position + direction*speed*Time.deltaTime);
+		if (Vector3.Distance(platform.position, destination.position) < speed * Time.deltaTime) {
+			SetDest(destination == start ? end : start);
+		}
+	}
+
+	void SetDest(Transform dest) {
+		destination = dest;
+		direction = (destination.position - platform.position).normalized;
+	}
+	void OnDrawGizmos() {
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(start.position,platform.localScale);
+		
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireCube(end.position,platform.localScale);
+	}
+
+}
