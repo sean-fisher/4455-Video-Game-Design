517ac8a51fd10b4a9fba5d4a1481da3337e1ee26
jbetancourt7 <jbeta51@hotmail.com>
2019-04-10 01:03:54 -0400

    add button script

---
+++ Assets/Enviroment/PuzzlePrefabs/Scripts/Button.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public bool toggle;
+
+    private bool status;
+
+    void OnTriggerEnter(Collider c)
+    {
+        status = toggle ? !status : true;
+        SetColor();
+    }
+    void OnTriggerExit(Collider c)
+    {
+        status = toggle ? status : false;
+    }
+
+    private void SetColor()
+    {
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("_Color");
+        // change color of button
+        if (status)
+        {
+            rend.material.SetColor("_Color", Color.green);
+        }
+        else
+        {
+            rend.material.SetColor("_Color", Color.red);
+        }
+    }
+
+}
