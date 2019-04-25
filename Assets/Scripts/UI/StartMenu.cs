606ae499a6ffb489979997e7f4bbda9b8cf152b2
sean-fisher <sean999fisher@gmail.com>
2019-04-25 14:43:42 -0400

    Controller ui input

--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -7,10 +7,13 @@ using UnityEngine.SceneManagement;
 public class StartMenu : Menu
 {
 
+    public UnityEngine.UI.Button backButton;
+
     // Start is called before the first frame update
     void Start()
     {
         AssignLevelButtons();
+        EnterMenu();
     }
 
     // Update is called once per frame
@@ -37,6 +40,7 @@ public class StartMenu : Menu
             Destroy(child.gameObject);
         }
         LevelName[] levels = LevelManager.Instance().levels;
+
         foreach (LevelName levelName in levels) {
             GameObject newLevelButton = GameObject.Instantiate(levelButtonTemplate, levelbuttonHolder);
             newLevelButton.GetComponentInChildren<Text>().text = levelName.visibleName;
@@ -44,6 +48,38 @@ public class StartMenu : Menu
                 Transitioner.Instance.LoadSceneWithFades(levelName.sceneName);
             });
         }
+
+        for (int i = 0; i < levels.Length; i++) {
+            UnityEngine.UI.Button b = levelbuttonHolder.GetChild(i).GetComponent<UnityEngine.UI.Button>();
+            Navigation nav = b.navigation;
+            nav.mode = Navigation.Mode.Explicit;
+
+            if (i == 0) {
+                nav.selectOnUp = backButton;
+
+                if (levels.Length == 1) {
+                    nav.selectOnDown = backButton;
+
+                } else {
+                    nav.selectOnDown = levelbuttonHolder.GetChild(i+1).GetComponent<UnityEngine.UI.Button>();
+
+                }
+            } else if (i == levels.Length - 1) {
+                nav.selectOnUp = levelbuttonHolder.GetChild(i-1).GetComponent<UnityEngine.UI.Button>();
+                nav.selectOnDown = backButton;
+            } else {
+                nav.selectOnUp = levelbuttonHolder.GetChild(i-1).GetComponent<UnityEngine.UI.Button>();
+                nav.selectOnDown = levelbuttonHolder.GetChild(i+1).GetComponent<UnityEngine.UI.Button>();
+            }
+
+            b.navigation = nav;
+        }
+
+        Navigation nav2 = backButton.navigation;
+        nav2.mode = Navigation.Mode.Explicit;
+        nav2.selectOnDown = levelbuttonHolder.GetChild(0).GetComponent<UnityEngine.UI.Button>();
+        nav2.selectOnUp = levelbuttonHolder.GetChild(levels.Length - 1).GetComponent<UnityEngine.UI.Button>();
+        backButton.navigation = nav2;
     }
 
     public void LoadLevel(int index) {
