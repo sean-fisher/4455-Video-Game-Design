66239e51959638a41a75e00ffe4666aaf75aadaf
Kyle C Perras <kperras3@gatech.edu>
2019-04-25 21:14:44 -0400

    redid moving platforms

---
+++ Assets/PlatformTranslator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTranslator : MonoBehaviour
+{
+    public Transform start;
+    public Transform end;
+    public float rideDuration;
+
+    float timer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = start.position;
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        transform.position = Vector3.Lerp(start.position, end.position, (timer / rideDuration));
+        if (Vector3.Distance(transform.position, end.position) < .1f)
+        {
+            transform.position = end.position;
+            Transform holder = end;
+            end = start;
+            start = holder;
+            timer = 0;
+        }
+    }
+}
